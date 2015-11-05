cdfd2d1ad70faffd1f54787f0b30f93162b9886f
Norman Rusch <norman.rusch@moccu.com>
2015-11-05 16:31:32 +0100

    Update documentation

--- src/tracking-registry/services/Registry.js
+++ src/tracking-registry/services/Registry.js
@@ -12,8 +12,8 @@ class Service {
 	 * Creates an instance of the registry service.
 	 *
 	 * @constructor
-	 * @param {object} options - The settings for the service.
-	 * @param {context} options.context - The reference to the
+	 * @param {object} options The settings for the service.
+	 * @param {context} options.context The reference to the
 	 *		backbone.geppetto context.
 	 */
 	constructor(options) {
@@ -34,10 +34,10 @@ class Service {
 	 * application into a single event. The event data will be updated to be
 	 * used in a single backbone.geppetto command.
 	 *
-	 * @param {string} fire - The event name which will be fired after the
+	 * @param {string} fire The event name which will be fired after the
 	 *		registered event appeared.
-	 * @param {string} eventName - The event name to listen and remap
-	 * @param {object} mapping - Defines the values which should be given to the
+	 * @param {string} eventName The event name to listen and remap
+	 * @param {object} mapping Defines the values which should be given to the
 	 *		track event call. Define here all values for the track event
 	 *		call (category, action, label, value). The registry can handle
 	 *		predefined values and dynamic value from functions or
@@ -81,8 +81,8 @@ class Service {
 	 * shorthand for .register()-call with settings for eventtacking in
 	 * google analytics.
 	 *
-	 * @param {string} eventName - Is the specific event name.
-	 * @param {object} mapping - Defines the values which should be given to the
+	 * @param {string} eventName Is the specific event name.
+	 * @param {object} mapping Defines the values which should be given to the
 	 *		track event call. Define here all values for the track event
 	 *		call (category, action, label, value). The registry can handle
 	 *		predefined values and dynamic value from functions or
@@ -114,8 +114,8 @@ class Service {
 	 * shorthand for .register()-call with settings for pageviewtacking in
 	 * google analytics.
 	 *
-	 * @param {string} eventName - Is the specific event name.
-	 * @param {object} mapping - Defines the values which should be given to the
+	 * @param {string} eventName Is the specific event name.
+	 * @param {object} mapping Defines the values which should be given to the
 	 *		track event call. Define here all values for the track event
 	 *		call (category, action, label, value). The registry can handle
 	 *		predefined values and dynamic value from functions or
@@ -147,8 +147,8 @@ class Service {
 	 * shorthand for .register()-call with settings for socialtracking in
 	 * google analytics.
 	 *
-	 * @param {string} eventName - Is the specific event name.
-	 * @param {object} mapping - Defines the values which should be given to the
+	 * @param {string} eventName Is the specific event name.
+	 * @param {object} mapping Defines the values which should be given to the
 	 *		track event call. Define here all values for the track event
 	 *		call (category, action, label, value). The registry can handle
 	 *		predefined values and dynamic value from functions or

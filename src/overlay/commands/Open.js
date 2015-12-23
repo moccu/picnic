b4dfb8716199f363ee35275b36a5a2e411742f4c
Norman Rusch <norman.rusch@moccu.com>
2015-12-23 10:02:13 +0100

    Refactored overlay open command

--- src/overlay/commands/Open.js
+++ src/overlay/commands/Open.js
@@ -2,7 +2,8 @@ import View from 'picnic/overlay/views/Overlay';
 
 
 var
-	WIRING_OVERLAY = 'overlay:view'
+	KEY_OVERLAY = 'overlay',
+	WIRING_OVERLAY = KEY_OVERLAY + ':view'
 ;
 
 
@@ -14,40 +15,38 @@ class Command {
 			view
 		;
 
+		// Handle creation of overlay:
 		if (context.hasWiring(WIRING_OVERLAY)) {
+			//...when an other overlay already exists, simply close the existing
+			// one and change the content later on...
 			view = context.getObject(WIRING_OVERLAY);
 			view.close();
 		} else {
+			//...create a new instance...
 			view = new View({context: context});
 			context.wireValue(WIRING_OVERLAY, view);
 		}
 
 		// Set optional settings:
-		this._optionReference(view, data);
+		view.reference = data.reference;
 
 		// Render overlay:
 		view.render(data.content);
-
-		// Set other optional settings:
-		this._optionClass(view, data);
-		this._optionClickblocker(view, data);
-	}
 		view.open();
 
-	_optionReference(view, data) {
-		view.reference = data.reference;
-	}
-
-	_optionClass(view, data) {
+		// Set other class names for overlay:
 		view.addClass(data.className);
+
+		// Enable clickblocker, when requested:
+		this._enableClickblocker(view, data);
 	}
 
-	_optionClickblocker(view, data) {
+	_enableClickblocker(view, data) {
 		var clickblocker = !!data.clickblocker;
 		view.hasClickblocker = clickblocker;
 
 		if (clickblocker) {
-			this.context.dispatch('clickblocker:open', {key: 'overlay'});
+			this.context.dispatch('clickblocker:open', {key: KEY_OVERLAY});
 		}
 	}
 }

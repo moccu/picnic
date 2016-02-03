57452cfda598cdf9c04941df3fea903add4d3991
Norman Rusch <norman.rusch@moccu.com>
2016-02-03 16:19:08 +0100

    Add core destroy command

---
+++ src/core/commands/Destroy.js
@@ -0,0 +1,148 @@
+import $ from 'jquery';
+import _ from 'underscore';
+
+
+/**
+ * A generic command to simply destroy view-modules by defining some
+ * settings. The destroyed view(s) will be removes to a given namespace. When
+ * there are no further views in the given namespace, the namespace will also
+ * be removed from the application context.
+ *
+ * The mandatory setting to provide is a *namespace*. To see how to use these
+ * settings take a look at the `get settings`-getter.
+ *
+ * When a view (which should be destroyed) has a `destroy()`-function, this
+ * function will be called before the view will be removed from the namespace.
+ *
+ * When wiring a destroy-command on a specific event and dispatch that event,
+ * you can pass a "root" element to the event as data to define a specific tree
+ * in the DOM where the views should be destroyed. Take a look at the examples to
+ * see how it works.
+ *
+ * @class Destroy-Command
+ * @example
+ *		import Destroy from 'picnic/core/commands/Destroy';
+ *
+ *		class Command extends Destroy {
+ *
+ *			get settings() {
+ *				return {
+ *					namespace: 'example:views'
+ *				};
+ *			}
+ *
+ * 		}
+ *
+ *		export default Command;
+ *
+ * @example
+ *		import SpecificDestroy from 'app/example/commands/Destroy';
+ *
+ *		// Pass a DOM-element as root in the eventData...
+ *		this.context.wireCommand('example1:event', SpecificDestroy);
+ *		this.context.dispatch('example1:event', {root: document.getElementById('example1')});
+ *
+ *		// Pass a jQuery-element as root in the eventData...
+ *		this.context.wireCommand('example2:event', SpecificDestroy);
+ *		this.context.dispatch('example2:event', {root: $('.example2')});
+ *
+ *		// Pass a selector-string as root in the eventData...
+ *		this.context.wireCommand('example3:event', SpecificDestroy);
+ *		this.context.dispatch('example3:event', {root: '.example3'});
+ */
+class Command {
+
+	/**
+	 * This getter returns the settings-object which is mandatory to destroy a
+	 * view-module. The mandatory setting to provide is a *namespace*.
+	 *
+	 * @example
+	 *		get settings() {
+	 *			return {
+	 *				namespace: 'example:views'
+	 *			};
+	 *		}
+	 */
+	get settings() {
+		return null;
+	}
+
+	/**
+	 * Contains all the logic to destroy the module(s). It's not ment to
+	 * overwrite this function.
+	 */
+	execute() {
+		this.preExecute();
+
+		var
+			views = [],
+			self = this,
+			data = self.eventData,
+			context = self.context,
+			settings = self.settings,
+			roots = $(data.root || document)
+		;
+
+		if (!settings.namespace) {
+			throw new Error('Define a namespace');
+		}
+
+		if (context.hasWiring(settings.namespace)) {
+			views = context.getObject(settings.namespace);
+
+			_.each(roots, function(root) {
+
+				var
+					index = 0,
+					view
+				;
+
+				while (index < views.length) {
+					view = views[index];
+
+					// Check, if current view is in given root element...
+					if ($.contains(root, view.el)) {
+
+						// Call destroy method on view...
+						if (typeof view.destroy === 'function') {
+							view.destroy();
+						}
+
+						// Remove view from wired views...
+						views.splice(index, 1);
+					} else {
+						index++;
+					}
+				}
+
+			}, this);
+
+
+			// When there are no existing views, remove the namespace...
+			if (!views.length) {
+				context.release(settings.namespace);
+			}
+		}
+
+		this.postExecute();
+	}
+
+	/**
+	 * Overwrite this function to add functionality before the initialization
+	 * of the module(s) start...
+	 */
+	preExecute() {
+		// Overwrite this...
+	}
+
+	/**
+	 * Overwrite this function to add functionality after the initialization
+	 * of the module(s)...
+	 */
+	postExecute() {
+		// Overwrite this...
+	}
+
+}
+
+export default Command;
---
+++ tests/core/commands/destroy.js
@@ -0,0 +1,221 @@
+/* global QUnit, sinon */
+import $ from 'jquery';
+import _ from 'underscore';
+import Backbone from 'backbone';
+import Geppetto from 'backbone.geppetto';
+import Destroy from 'picnic/core/commands/Destroy';
+import Fixure from 'tests/core/commands/fixures/destroy.html!text';
+
+
+function create(options, context, eventData) {
+	class Command extends Destroy {
+		get settings() {
+			return options;
+		}
+	}
+
+	var instance = new Command();
+	instance.context = context;
+	instance.eventData = $.extend({}, eventData);
+	instance.dispatch = function() {
+		context.dispatch.apply(context, arguments);
+	};
+
+	return instance;
+}
+
+QUnit.module('The core destroy command', {
+	beforeEach: function() {
+		var self = this;
+		this.root = $('#qunit-fixture');
+		$(Fixure).appendTo(this.root);
+		this.context = new Geppetto.Context();
+		this.views = [];
+		this.elements = this.root.find('.test').each(function() {
+			self.views.push(new Backbone.View({
+				el: this,
+				context: self.context
+			}));
+		});
+
+		this.context.wireValue('test:views', this.views);
+	}
+});
+
+QUnit.test(
+	'should fail when no settings are defined',
+	function(assert) {
+		var
+			instance = create(
+				null,
+				this.context
+			)
+		;
+
+		assert.throws(function() {
+			instance.execute();
+		});
+	}
+);
+
+QUnit.test(
+	'should fail when no namespace is defined',
+	function(assert) {
+		var
+			instance = create(
+				{},
+				this.context
+			)
+		;
+
+		assert.throws(function() {
+			instance.execute();
+		}, new Error('Define a namespace'));
+	}
+);
+
+QUnit.test(
+	'should remove views from wired list',
+	function(assert) {
+		var
+			instance = create(
+				{namespace: 'test:views'},
+				this.context
+			)
+		;
+
+		instance.execute();
+		assert.equal(this.views.length, 0);
+	}
+);
+
+QUnit.test(
+	'should remove namespace when no further views exists',
+	function(assert) {
+		var
+			instance = create(
+				{namespace: 'test:views'},
+				this.context
+			)
+		;
+
+		instance.execute();
+		assert.ok(!this.context.hasWiring('test:views'));
+	}
+);
+
+QUnit.test(
+	'should remove views in given root when root is a DOM element',
+	function(assert) {
+		var
+			instance = create(
+				{namespace: 'test:views'},
+				this.context,
+				{root: document.getElementById('group1')}
+			)
+		;
+
+		instance.execute();
+		assert.equal(this.views.length, 2);
+		assert.equal(this.views[0].el, document.getElementById('test1'));
+		assert.equal(this.views[1].el, document.getElementById('test3'));
+		assert.ok(this.context.hasWiring('test:views'));
+	}
+);
+
+QUnit.test(
+	'should remove views in given root when root is a jQuery-element',
+	function(assert) {
+		var
+			instance = create(
+				{namespace: 'test:views'},
+				this.context,
+				{root: this.root.find('.group')}
+			)
+		;
+
+		instance.execute();
+		assert.equal(this.views.length, 1);
+		assert.equal(this.views[0].el, document.getElementById('test1'));
+		assert.ok(this.context.hasWiring('test:views'));
+	}
+);
+
+QUnit.test(
+	'should remove views in given root when root is a selector-string',
+	function(assert) {
+		var
+			instance = create(
+				{namespace: 'test:views'},
+				this.context,
+				{root: '#group2'}
+			)
+		;
+
+		instance.execute();
+		assert.equal(this.views.length, 2);
+		assert.equal(this.views[0].el, document.getElementById('test1'));
+		assert.equal(this.views[1].el, document.getElementById('test2'));
+		assert.ok(this.context.hasWiring('test:views'));
+	}
+);
+
+QUnit.test(
+	'should call destroy()-function on views',
+	function(assert) {
+		var
+			instance = create(
+				{namespace: 'test:views'},
+				this.context,
+				{root: this.root.find('.group')}
+			),
+			callbacks = []
+		;
+
+		_.each(this.views, function(view) {
+			var callback = sinon.spy();
+			view.destroy = callback;
+			callbacks.push(callback);
+		}, this);
+
+
+		instance.execute();
+		assert.ok(callbacks[0].notCalled);
+		assert.ok(callbacks[1].calledOnce);
+		assert.ok(callbacks[2].calledOnce);
+	}
+);
+
+QUnit.test(
+	'should call preExecute() on instance',
+	function(assert) {
+		var
+			instance = create(
+				{namespace: 'test:views'},
+				this.context
+			)
+		;
+
+		instance.preExecute = sinon.spy();
+		instance.execute();
+
+		assert.ok(instance.preExecute.calledOnce);
+	}
+);
+
+QUnit.test(
+	'should call postExecute() on instance',
+	function(assert) {
+		var
+			instance = create(
+				{namespace: 'test:views'},
+				this.context
+			)
+		;
+
+		instance.postExecute = sinon.spy();
+		instance.execute();
+
+		assert.ok(instance.postExecute.calledOnce);
+	}
+);

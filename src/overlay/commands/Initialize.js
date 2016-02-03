28336fda2c431c91e1a4d8cda0a107cd190a5ed3
Norman Rusch <norman.rusch@moccu.com>
2016-02-03 17:47:22 +0100

    Add possibility to initialize the overlay module only once

--- src/overlay/commands/Initialize.js
+++ src/overlay/commands/Initialize.js
@@ -10,6 +10,11 @@ import CloseCommand from 'picnic/overlay/commands/Close';
  * the overlay(s). The events to trigger those commands are 'overlay:open' and
  * 'overlay:close'.
  *
+ * This initialize command offers the possibility, when executing this command
+ * more than once, the initialization process itself is performed only once. So
+ * there are no duplicate wirings for the 'overlay:open'- or
+ * 'overlay:close'-event.
+ *
  * @class Overlay
  * @example
  *		// Open an overlay:
@@ -50,10 +55,38 @@ import CloseCommand from 'picnic/overlay/commands/Close';
 class Command {
 
 	execute() {
-		this.context.wireCommand('overlay:open', OpenCommand);
-		this.context.wireCommand('overlay:close', CloseCommand);
+		if (!this.called) {
+			this.context.wireCommand('overlay:open', OpenCommand);
+			this.context.wireCommand('overlay:close', CloseCommand);
+		}
+
+		this.incCount();
+	}
+
+	get called() {
+		return Command.called;
+	}
+
+	incCount() {
+		Command.incCount();
 	}
 
 }
+
+// Adding the callcount feature into the command-class:
+// -----------------------------------------------------------------------------
+var __callcount = 0;
+
+Object.defineProperty(Command, 'callcount', {
+	get: function() { return __callcount; }
+});
+
+Object.defineProperty(Command, 'called', {
+	get: function() { return __callcount > 0; }
+});
+
+Command.resetCount = function() { __callcount = 0; };
+
+Command.incCount = function() { __callcount++; };
 
 export default Command;

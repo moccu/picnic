3df4314692fb498727fb3a95c435e599ab2b9b5c
Dimitri Steinel <dimitri.steinel@moccu.com>
2016-10-24 16:11:51 +0200

    tabfocus view: refactor tests

--- tests/tabfocus/views/tabfocus.js
+++ tests/tabfocus/views/tabfocus.js
@@ -36,9 +36,13 @@ QUnit.test(
 );
 
 QUnit.test(
-	'should focus focusable elements on tab',
+	'should focus default focusable elements on tab',
 	function(assert) {
-		this.view.render();
+		this.view = new Tabfocus({
+			el: this.root,
+			context: this.context,
+			selector: 'body'
+		}).render();
 
 		this.view.$el.find('a').eq(1).focus().trigger(KEYUP);
 		assert.ok(this.view.$el.find('a').eq(1).hasClass(CLASS_FOCUS));
@@ -54,6 +58,51 @@ QUnit.test(
 
 		this.view.$el.find('div').eq(1).focus().trigger(KEYUP);
 		assert.ok(this.view.$el.find('div').eq(1).hasClass(CLASS_FOCUS));
+	}
+);
+
+QUnit.test(
+	'should change focus class',
+	function(assert) {
+		var
+			NEWCLASS = 'foobar'
+		;
+
+		this.view = new Tabfocus({
+			el: this.root,
+			context: this.context,
+			classFocus: NEWCLASS
+		}).render();
+
+
+		this.view.$el.find('a').eq(3).focus().trigger(KEYUP);
+		assert.ok(this.view.$el.find('a').eq(3).hasClass(NEWCLASS));
+
+		this.view.$el.find('a').eq(1).focus().trigger(KEYUP);
+		assert.ok(this.view.$el.find('a').eq(1).hasClass(NEWCLASS));
+
+		this.view.$el.find('div').eq(1).focus().trigger(KEYUP);
+		assert.ok(this.view.$el.find('div').eq(1).hasClass(NEWCLASS));
+	}
+);
+
+QUnit.test(
+	'should change focusable selector',
+	function(assert) {
+		this.view = new Tabfocus({
+			el: this.root,
+			context: this.context,
+			selectorFocusable: 'input'
+		}).render();
+
+		this.view.$el.find('input').eq(0).focus().trigger(KEYUP);
+		assert.ok(this.view.$el.find('input').eq(0).hasClass(CLASS_FOCUS));
+
+		this.view.$el.find('input').eq(1).focus().trigger(KEYUP);
+		assert.ok(this.view.$el.find('input').eq(1).hasClass(CLASS_FOCUS));
+
+		this.view.$el.find('a').eq(3).focus().trigger(KEYUP);
+		assert.notOk(this.view.$el.find('a').eq(3).hasClass(CLASS_FOCUS));
 	}
 );
 
@@ -108,58 +157,20 @@ QUnit.test(
 );
 
 QUnit.test(
-	'should unbind events on destroy',
-	function(assert) {
-		this.view.render();
-		this.view.destroy();
-		assert.notOk($._data($('#qunit-fixture')[0], 'events'));
-	}
-);
-
-QUnit.test(
-	'should change focus class',
-	function(assert) {
-		var
-			newClass = 'foobar'
-		;
-
-		this.view = new Tabfocus({
-			el: this.root,
-			context: this.context,
-			classFocus: newClass
-		}).render();
-
-		this.view.$el.find('a').eq(3).focus().trigger(KEYUP);
-		assert.ok(this.view.$el.find('a').eq(3).hasClass(newClass));
-	}
-);
-
-QUnit.test(
-	'should change target',
-	function(assert) {
-		this.view = new Tabfocus({
-			el: this.root,
-			context: this.context,
-			selectorFocusable: 'input'
-		}).render();
-
-		this.view.$el.find('input').eq(0).focus().trigger(KEYUP);
-		assert.ok(this.view.$el.find('input').eq(0).hasClass(CLASS_FOCUS));
-
-		this.view.$el.find('input').eq(1).focus().trigger(KEYUP);
-		assert.ok(this.view.$el.find('input').eq(1).hasClass(CLASS_FOCUS));
-
-		this.view.$el.find('a').eq(3).focus().trigger(KEYUP);
-		assert.notOk(this.view.$el.find('a').eq(3).hasClass(CLASS_FOCUS));
-	}
-);
-
-QUnit.test(
 	'should not focus disabled button',
 	function(assert) {
 		this.view.render();
 
 		this.view.$el.find('.disable').eq(0).focus().trigger(KEYUP);
 		assert.notOk(this.view.$el.find('.disable').eq(0).hasClass(CLASS_FOCUS));
+	}
+);
+
+QUnit.test(
+	'should unbind events on destroy',
+	function(assert) {
+		this.view.render();
+		this.view.destroy();
+		assert.notOk($._data($('#qunit-fixture')[0], 'events'));
 	}
 );

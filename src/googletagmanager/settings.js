cfef79993681b35861f9fd4bf1f367913339cbae
Norman Rusch <norman.rusch@moccu.com>
2016-11-30 18:44:04 +0100

    Update googletagmanager module * Fix custom layer setting * Add initial layer push on initialization * Add shared settings * Store layer in namespace

---
+++ src/googletagmanager/settings.js
@@ -0,0 +1,9 @@
+export default {
+	enabled: true,
+	source: '//www.googletagmanager.com/gtm.js?id=',
+	layer: 'dataLayer',
+	initialLayerPushs: [],
+	trackeventName: 'uaevent',
+	trackpageviewName: 'updatevirtualpath',
+	tracksocialName: 'socialInt'
+};

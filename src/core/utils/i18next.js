6b0ee1acfe418d1056142b820dc2a1a5a20d2b54
Sascha Künstler <sascha.kuenstler@moccu.com>
2019-02-25 09:23:44 +0100

    Fix import

--- src/core/utils/i18next.js
+++ src/core/utils/i18next.js
@@ -1,6 +1,6 @@
 const i18next = window.i18next || {};
 
-i18next.t = window.i18next.t || function(label, props) {
+i18next.t = (window.i18next) ? window.i18next.t : function(label, props) {
 	if (typeof props === 'object') {
 		Object.keys(props).forEach(function(key) {
 			const re = new RegExp('{' + '{ ?' + key + ' ?}' + '}', 'g');

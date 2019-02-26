377f9fabcf09b1f686b6cf1beb98fedfa5c30395
Sascha Künstler <sascha.kuenstler@moccu.com>
2019-02-26 10:28:49 +0100

    Update tests

--- src/core/utils/i18next.js
+++ src/core/utils/i18next.js
@@ -1,14 +1,18 @@
 const i18next = window.i18next || {};
 
-i18next.t = (window.i18next) ? window.i18next.t : function(label, props) {
-	if (label && typeof props === 'object') {
-		Object.keys(props).forEach(function(key) {
-			const re = new RegExp('{{ ?' + key + ' ?}}', 'g');
-			label = label.replace(re, props[key]);
-		});
-	}
+i18next.t = function(label, props) {
+	if (window.i18next && typeof window.i18next.t === 'function') {
+		return window.i18next.t(label, props);
+	} else {
+		if (label && typeof props === 'object') {
+			Object.keys(props).forEach(function(key) {
+				const re = new RegExp('{{ ?' + key + ' ?}}', 'g');
+				label = label.replace(re, props[key]);
+			});
+		}
 
-	return label;
+		return label;
+	}
 };
 
 export {i18next};

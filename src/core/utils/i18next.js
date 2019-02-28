c3678d6d320cc2f2c9369cf60d51d3973179d9ff
Sascha Künstler <sascha.kuenstler@moccu.com>
2019-02-28 10:18:26 +0100

    Avoid infinite loop in 18next polyfill

--- src/core/utils/i18next.js
+++ src/core/utils/i18next.js
@@ -1,18 +1,18 @@
 const i18next = window.i18next || {};
 
-i18next.t = function(label, props) {
-	if (window.i18next && typeof window.i18next.t === 'function') {
+i18next.t = (window.i18next) ? window.i18next.t : function(label, props) {
+	if (window.i18next && i18next.t.toString() !== window.i18next.t.toString()) {
 		return window.i18next.t(label, props);
-	} else {
-		if (label && typeof props === 'object') {
-			Object.keys(props).forEach(function(key) {
-				const re = new RegExp('{{ ?' + key + ' ?}}', 'g');
-				label = label.replace(re, props[key]);
-			});
-		}
-
-		return label;
 	}
+
+	if (label && typeof props === 'object') {
+		Object.keys(props).forEach(function(key) {
+			const re = new RegExp('{{ ?' + key + ' ?}}', 'g');
+			label = label.replace(re, props[key]);
+		});
+	}
+
+	return label;
 };
 
 export {i18next};
